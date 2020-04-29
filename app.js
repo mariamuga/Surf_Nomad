1d0dc63912a7c00bd91c4b56687b1ae60857a065
clarafmiranda <clarafmiranda.dev@gmail.com>
2020-04-29 21:58:45 +0200

    little change

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
 const app_name = require("./package.json").name;
-const debug = require("debug")(
-  `${app_name}:${path.basename(__filename).split(".")[0]}`
-);
+const debug = require("debug")(`${app_name}:${path.basename(__filename).split(".")[0]}`);
 const User = require("./models/User");
 // Login & Signup incription
 const session = require("express-session");
@@ -32,9 +30,7 @@ mongoose.set("useUnifiedTopology", true);
 mongoose
   .connect("mongodb://localhost/Surf_Nomad", { useNewUrlParser: true })
   .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
   .catch((err) => {
     console.error("Error connecting to mongo", err);
